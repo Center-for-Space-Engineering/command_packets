db26433a69799f728c71e3a61f5a8458df55266b
CSE  Ground Station <nathan.tipton@arcfield.com>
2024-05-24 13:25:03 -0600

    working on updates, working with yaml for packet processing and linting

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+'''
+    This modules contains function to help us procerss packets.
+'''
 def ccsds_crc16(data : bytearray):
     '''
         Make crc16/CCITT-FALSE check sum for the CCSDS packet.
@@ -9,11 +12,10 @@ def ccsds_crc16(data : bytearray):
 
 
     for byte in data:
-        # print(hex(crc & 0xFFFF))
         crc ^= byte << 8
         for _ in range(0,8):
-            if (crc & 0x8000):
+            if crc & 0x8000:
                 crc = (crc << 1) ^ poly
             else:
                 crc = crc << 1
-    return crc & 0xFFFF 
+    return crc & 0xFFFF
